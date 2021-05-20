d879a0c857665668ce34c25af71881738d414550
aiden mackean <ajmackean@icloud.com>
2021-05-20 17:41:17 -0400

    inclusion of main

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
-/* test commit for the application main
+/* test commit for the application main */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+
+    printf("test output");
+
+    return EXIT_SUCCESS;
+
+
+}
